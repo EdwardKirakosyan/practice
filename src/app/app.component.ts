084c298e5533ff7c96122c34359636fa428e5564
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-21 19:07:38 +0400

    с

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,11 +16,37 @@ import {
   imports: [CommonModule, RouterOutlet, DemoComponent, ReactiveFormsModule],
   styleUrl: './app.component.scss',
   template: `
-    <div>
-      
+  <div>
+    <button (click)="onServ()">service</button>
+    <button (click)="onInfo()">info</button>
+    <button (click)="onPrivacy()">privacy</button>
+    <button (click)="onUser()">user</button>
+    <br>
+    <div [ngSwitch]="tab">
+      <h1 *ngSwitchCase="'service'">vvvvvv</h1>
+      <h1 *ngSwitchCase="'info'">ssss</h1>
+      <h1 *ngSwitchCase="'privacy'">aaaaa</h1>
+      <h1 *ngSwitchCase="'user'">тттттт</h1>
     </div>
+  </div>
   `,
 })
 export class AppComponent {
+  tab = ''
 
+  onServ() {
+    this.tab = 'service'
+  }
+
+  onInfo() {
+    this.tab = 'info'
+  }
+
+  onPrivacy() {
+    this.tab = 'privacy'
+  }
+
+  onUser() {
+    this.tab = 'user'
+  }
 }
