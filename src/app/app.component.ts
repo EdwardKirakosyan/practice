084f6083f5ed13ffd7c42106f2ff820405d9a983
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-22 12:02:56 +0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,17 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { DemoComponent } from "./demo/demo.component";
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators
-} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DemoComponent, ReactiveFormsModule],
+  imports: [CommonModule],
   styleUrl: './app.component.scss',
   template: `
   <div>
-    <button (click)="onServ()">service</button>
-    <button (click)="onInfo()">info</button>
-    <button (click)="onPrivacy()">privacy</button>
-    <button (click)="onUser()">user</button>
-    <br>
-    <div [ngSwitch]="tab">
-      <h1 *ngSwitchCase="'service'">vvvvvv</h1>
-      <h1 *ngSwitchCase="'info'">ssss</h1>
-      <h1 *ngSwitchCase="'privacy'">aaaaa</h1>
-      <h1 *ngSwitchCase="'user'">тттттт</h1>
-    </div>
+
   </div>
   `,
 })
 export class AppComponent {
-  tab = ''
 
-  onServ() {
-    this.tab = 'service'
-  }
-
-  onInfo() {
-    this.tab = 'info'
-  }
-
-  onPrivacy() {
-    this.tab = 'privacy'
-  }
-
-  onUser() {
-    this.tab = 'user'
-  }
 }
