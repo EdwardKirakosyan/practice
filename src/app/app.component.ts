ffc8808fcf34942e92e92dd0b6c98fbd54cf5a87
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-22 12:59:19 +0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
+import { DemoComponent } from "./demo/demo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
   styleUrl: './app.component.scss',
   template: `
-  <div>
 
-  </div>
   `,
+  imports: [CommonModule, FormsModule, DemoComponent]
 })
 export class AppComponent {
 
