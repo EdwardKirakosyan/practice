b9b3d0c15c13537cba959a6e797e034f4b7118ba
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-01-25 15:21:56 +0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from "@angular/forms";
 import { DemoComponent } from "./demo/demo.component";
 import { HttpClient, HttpClientModule, HttpHeaders } from "@angular/common/http";
 import { Task } from "./Task";
+import { map } from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -16,16 +17,34 @@ import { Task } from "./Task";
     </div>
   `,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   http: HttpClient = inject(HttpClient)
+
+  ngOnInit(): void {
+    this.fetchAll()
+  }
 
   createTask(data: Task) {
     const headers = new HttpHeaders({ 'header': 'ssss' })
-    this.http.post<{ name: string }>('https://angu-8612c-default-rtdb.europe-west1.firebasedatabase.app/tasks.json', data, {
-      headers: headers
-    })
+    this.http.post<{ name: string }>('https://angu-8612c-default-rtdb.europe-west1.firebasedatabase.app/tasks.json', data, { headers: headers })
       .subscribe((res) => {
         console.log(res)
+      })
+  }
+
+  private fetchAll() {
+    this.http.get<{ [key: string]: Task }>('https://angu-8612c-default-rtdb.europe-west1.firebasedatabase.app/tasks.json')
+      .pipe(map((res) => {
+        let tasks = []
+        for (let key in res) {
+          if (res.hasOwnProperty(key)) {
+            tasks.push({ ...res[key], id: key })
+          }
+        }
+        return tasks
+      }))
+      .subscribe((tasks) => {
+        console.log(tasks)
       })
   }
 }
